2c8f9a841cd2f6ad8ec3acc56a1a22d5abf20ea4
Anatolii <omelchenko20@gmail.com>
2024-04-25 14:48:25 +0300

    feat: added postgres db support using docker image

--- src/main/java/com/clearsolutions/usermanager/model/User.java
+++ src/main/java/com/clearsolutions/usermanager/model/User.java
@@ -1,9 +1,6 @@
 package com.clearsolutions.usermanager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,6 +18,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
